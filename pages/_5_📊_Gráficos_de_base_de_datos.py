790f8f09308d547e9ef777b3fbf99638050f1872
V1d0K <fran_tco99@hotmail.com>
2022-11-17 20:28:33 +0100

    Grafica

--- pages/_5_📊_Gráficos_de_base_de_datos.py
+++ pages/_5_📊_Gráficos_de_base_de_datos.py
@@ -25,9 +25,11 @@ select = run_query("SELECT year, COUNT(game) from game_sales group by year;")
 # Print results.
 dataframe =pd.DataFrame(rows)
 df2 = pd.DataFrame(select)
+df2.columns=['año','juegos_por_año' ]
+df2.set_index('año', inplace=True)
+
 st.write(dataframe)
 st.write(df2)
-fig=df2.hist('juegos')
-st.pyplot('fig')
+st.bar_chart(fig)
 
 # game_df.apply(lambda x: x.sort_values(by = '4', ascending=False)).head(10)
