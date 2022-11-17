d27df819ee5dd2ce2df5ba335ad786572ded2f6d
V1d0K <fran_tco99@hotmail.com>
2022-11-17 19:03:55 +0100

    Actualizacion tabla game ssales

---
+++ pages/_5_📊_Gráficos_de_base_de_datos.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(st.secrets["db_connection_str"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from game_sales")
+
+# Print results.
+game_df=pd.DataFrame(rows)
+st.dataframe(game_df)
